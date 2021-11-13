1080c1ad53ef9214194b20d4c056087399aa4e91
astrviktor <astrviktor@gmail.com>
2021-11-13 11:40:23 +0300

    hw07 done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,103 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrEmptyPath             = errors.New("empty path")
+	ErrEmptySize             = errors.New("empty size")
 )
 
+// буффер для копирования файла.
+const bufferMaxSize = 1024 * 1024 * 10
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	// проверка на пустые пути к файлам
+	if fromPath == "" || toPath == "" {
+		return ErrEmptyPath
+	}
+
+	// проверка файла fromPath
+	fileInfo, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+	if fileInfo.IsDir() {
+		return ErrUnsupportedFile
+	}
+	size := fileInfo.Size()
+	if size == 0 {
+		return ErrEmptySize
+	}
+
+	// если offset больше, чем размер файла - невалидная ситуация;
+	if offset > size {
+		return ErrOffsetExceedsFileSize
+	}
+
+	copySize := size - offset
+	if limit != 0 && copySize > limit {
+		copySize = limit
+	}
+
+	// определение буфера для прогресса копирования в процентах (%)
+	bufferSize := copySize / 4
+	switch {
+	case bufferSize == 0:
+		bufferSize = 1
+	case bufferSize > bufferMaxSize:
+		bufferSize = bufferMaxSize
+	}
+	buf := make([]byte, bufferSize)
+
+	// файл from
+	fi, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer fi.Close()
+
+	_, err = fi.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+
+	// файл to
+	fo, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer fo.Close()
+
+	// процесс копирования
+	copyLeft := copySize
+	var copyCount int64
+
+	for copyCount != copySize {
+		if copyLeft < bufferSize {
+			buf = buf[:copyLeft]
+		}
+
+		n, err := io.ReadFull(fi, buf)
+		if err != nil && !errors.Is(err, io.ErrUnexpectedEOF) && !errors.Is(err, io.EOF) {
+			return err
+		}
+
+		_, err = fo.Write(buf[:n])
+		if err != nil {
+			return err
+		}
+		copyCount += int64(n)
+
+		// прогресс
+		copyLeft -= bufferSize
+		fmt.Println("progress", 100*copyCount/copySize, "%")
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,93 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("test open testdata/input.txt", func(t *testing.T) {
+		f, err := os.Open("testdata/input.txt")
+		if err != nil {
+			t.Fail()
+		}
+		defer f.Close()
+		require.NoError(t, err)
+	})
+
+	tests := []struct {
+		fromPath string
+		toPath   string
+		offset   int64
+		limit    int64
+	}{
+		{fromPath: "input.txt", toPath: "out_offset0_limit0.txt", offset: 0, limit: 0},
+		{fromPath: "input.txt", toPath: "out_offset0_limit10.txt", offset: 0, limit: 10},
+		{fromPath: "input.txt", toPath: "out_offset0_limit1000.txt", offset: 0, limit: 1000},
+		{fromPath: "input.txt", toPath: "out_offset0_limit10000.txt", offset: 0, limit: 10000},
+		{fromPath: "input.txt", toPath: "out_offset100_limit1000.txt", offset: 100, limit: 1000},
+		{fromPath: "input.txt", toPath: "out_offset6000_limit1000.txt", offset: 6000, limit: 1000},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.toPath, func(t *testing.T) {
+			err := Copy(filepath.Join("testdata/", tc.fromPath), filepath.Join("/tmp/", tc.toPath), tc.offset, tc.limit)
+			require.NoError(t, err)
+
+			testData, err := ioutil.ReadFile(filepath.Join("testdata/", tc.toPath))
+			require.NoError(t, err)
+
+			resultData, err := ioutil.ReadFile(filepath.Join("/tmp/", tc.toPath))
+			require.NoError(t, err)
+
+			require.Equal(t, testData, resultData)
+
+			err = os.Remove(filepath.Join("/tmp/", tc.toPath))
+			require.NoError(t, err)
+		})
+	}
+
+	t.Run("test copy /dev/random", func(t *testing.T) {
+		err := Copy("/dev/random", "/tmp/random.txt", 0, 10)
+
+		require.Error(t, err)
+		require.True(t, errors.Is(err, ErrEmptySize))
+	})
+
+	t.Run("test empty paths", func(t *testing.T) {
+		err := Copy("", "", 0, 0)
+
+		require.Error(t, err)
+		require.True(t, errors.Is(err, ErrEmptyPath))
+
+		err = Copy("", "/tmp/123", 0, 0)
+
+		require.Error(t, err)
+		require.True(t, errors.Is(err, ErrEmptyPath))
+
+		err = Copy("/tmp/123", "", 0, 0)
+
+		require.Error(t, err)
+		require.True(t, errors.Is(err, ErrEmptyPath))
+	})
+
+	t.Run("test offset exceeds file size", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "/tmp/out_offset10000_limit0.txt", 10000, 0)
+
+		require.Error(t, err)
+		require.True(t, errors.Is(err, ErrOffsetExceedsFileSize))
+	})
+
+	t.Run("test dir instead of file", func(t *testing.T) {
+		err := Copy("testdata", "/tmp/out_offset0_limit0.txt", 0, 0)
+
+		require.Error(t, err)
+		require.True(t, errors.Is(err, ErrUnsupportedFile))
+	})
 }
